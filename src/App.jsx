f8b3bc5020238ee7681e6afbae318396dfd0f743
Suraj Honmane <surajhonmane18@gmail.com>
2025-01-31 01:20:36 +0530

    Login and Admin Done

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import LandingPage from './components/LandingPage';
 import PrintingService from './components/PrintingServices';
 import PrintSubmission from './components/PrintingSubmission';
 import CustomerLogin from './components/authentication/CustomerLogin';
-import AdminLogin from './components/authentication/OwnerSignIn'
+import AdminLogin from './components/authentication/OwnerSignIn';
 
 import Homepage from './components/Homepage';
+import Dashboard from './components/owner/dashboard';
+import Canteen from './components/owner/Canteen/Canteen';
+import CC from './components/owner/CC/CC';
 
 
 
@@ -23,6 +26,9 @@ const router = createBrowserRouter(
       <Route path="adminlogin" element={<AdminLogin />} />
 
       <Route path="homepage" element={<Homepage />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="canteen" element={<Canteen />} />
+      <Route path="cc" element={<CC />} />
 
       <Route path="print" element={<PrintingService />} />
       <Route path="print-submission" element={<PrintSubmission />} />
